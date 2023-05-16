812d134120aa07ef93428b6ae5a6c600f617b5e9
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-05-16 16:14:35 -0700

    Toggle Functionality

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const toggleMenu = document.getElementById('toggleMenu');
+const toggleMenu = document.getElementById('Togglebtn');
 toggleMenu.addEventListener('click', () => {
+  const toggleMenu = document.getElementById('navigation');
   toggleMenu.classList.toggle('container');
-})
-/*
-function closeMenu() {
-  const close = document.getElementById('navigation');
-  close.classList.remove('container');
-}
-
-
-function itemMenu() {
-  const Navitem = document.getElementById('navigation');
-  Navitem.classList.remove('container');
-}
-
-*/
+});
+const CloseMenu = document.getElementById('Closebtn');
+CloseMenu.addEventListener('click', () => {
+  const CloseMenu = document.getElementById('navigation');
+  CloseMenu.classList.remove('container');
+});
+const Navitems = document.querySelectorAll('.Navitem');
+Navitems.forEach((Navitem) => {
+  Navitem.addEventListener('click', () => {
+    const navigation = document.getElementById('navigation');
+    navigation.classList.remove('container');
+  });
+});
