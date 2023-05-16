230d9559302e866855145fdf594b1b05de5b88c3
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-05-16 06:57:59 -0700

    Toggle

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const Menu = document.getElementById('navigation');
+    Menu.classList.toggle('container');
+}
+
+function closeMenu() {
+    const Close_Menu = document.getElementById('navigation');
+    Close_Menu.classList.remove('container');
+  }
