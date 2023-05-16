3d174ff444842fca279ea2c3ebc0d13170534328
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-05-16 13:58:41 -0700

    Commit

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-function toggle() {
-  const toggle = document.getElementById('navigation');
-  toggle.classList.toggle('container');
-}
-
-function close() {
+const toggleMenu = document.getElementById('toggleMenu');
+toggleMenu.addEventListener('click', () => {
+  toggleMenu.classList.toggle('container');
+})
+/*
+function closeMenu() {
   const close = document.getElementById('navigation');
   close.classList.remove('container');
 }
+
 
 function itemMenu() {
   const Navitem = document.getElementById('navigation');
   Navitem.classList.remove('container');
 }
+
+*/
