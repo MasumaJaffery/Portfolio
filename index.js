88bb8dbe24da353bb2f3785bef938ead802735f7
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-05-17 12:16:37 -0700

    JS Objects

--- index.js
+++ index.js
@@ -15,3 +15,41 @@ Navitems.forEach((Navitem) => {
     navigation.classList.remove('container');
   });
 });
+//JS Pop Functionality<---->(JS Objects)
+const Projects = [{
+  img: 'Images/Snapshoot Portfolio.png',
+  title: 'Tonic',
+  skill: ['CANOPY','Back End Dev', '2023'],
+  para: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  tech: ['HTML','CSS','JavaScript'],
+  source: 'https://github.com/MasumaJaffery/Portfolio',
+  livelink: 'https://masumajaffery.github.io/Portfolio/'
+},
+{
+  img: 'Images/Snapshoot Portfolio (1).png',
+  title: 'Multi-Post Stories',
+  skill: ['CANOPY','Back End Dev','2023'],
+  para: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  tech: ['HTML','CSS','JavaScript'],
+  source:'https://github.com/MasumaJaffery/Portfolio',
+  livelink: 'https://masumajaffery.github.io/Portfolio/'
+},
+{
+  img: 'Images/Snapshoot Portfolio (2).png',
+  title: 'Tonic',
+  skill: ['CANOPY','Back End Dev', '2023'],
+  para: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  tech: ['HTML','CSS','JavaScript'],
+  source: 'https://github.com/MasumaJaffery/Portfolio',
+  livelink: 'https://masumajaffery.github.io/Portfolio/'
+},
+{
+  img: 'Images/Snapshoot Portfolio (3).png',
+  title: 'Multi-Post Stories',
+  skill: ['CANOPY','Back End Dev','2023'],
+  para: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  tech: ['HTML','CSS','JavaScript'],
+  source:'https://github.com/MasumaJaffery/Portfolio',
+  livelink: 'https://masumajaffery.github.io/Portfolio/'
+}];
+// Use this Objects in Function Below
