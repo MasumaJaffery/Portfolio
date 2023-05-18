31fba44462cc38ac8f21c00f41e439537b14f36f
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-05-18 13:40:28 -0700

    Mobile-PopUp

--- index.js
+++ index.js
@@ -17,37 +17,41 @@ Navitems.forEach((Navitem) => {
 });
 //JS Pop Functionality<---->(JS Objects)
 const Projects = [{
-  img: 'Images/Snapshoot Portfolio.png',
+  img: 'Images/Snapshoot Portfolio-Desktop.png',
   title: 'Tonic',
   skill: ['CANOPY','Back End Dev', '2023'],
   para: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  parapop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries but also the leap into electronic typesetting remaining essent',
   tech: ['HTML','CSS','JavaScript'],
   source: 'https://github.com/MasumaJaffery/Portfolio',
   livelink: 'https://masumajaffery.github.io/Portfolio/'
 },
 {
-  img: 'Images/Snapshoot Portfolio (1).png',
+  img: 'Images/Snapshoot Portfolio-Desktop-2.png',
   title: 'Multi-Post Stories',
   skill: ['CANOPY','Back End Dev','2023'],
   para: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  parapop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries but also the leap into electronic typesetting remaining essent',
   tech: ['HTML','CSS','JavaScript'],
   source:'https://github.com/MasumaJaffery/Portfolio',
   livelink: 'https://masumajaffery.github.io/Portfolio/'
 },
 {
-  img: 'Images/Snapshoot Portfolio (2).png',
+  img: 'Images/Snapshoot Portfolio-Desktop-3.png',
   title: 'Tonic',
   skill: ['CANOPY','Back End Dev', '2023'],
   para: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  parapop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries but also the leap into electronic typesetting remaining essent',
   tech: ['HTML','CSS','JavaScript'],
   source: 'https://github.com/MasumaJaffery/Portfolio',
   livelink: 'https://masumajaffery.github.io/Portfolio/'
 },
 {
-  img: 'Images/Snapshoot Portfolio (3).png',
+  img: 'Images/Snapshoot Portfolio-Desktop-4.png',
   title: 'Multi-Post Stories',
   skill: ['CANOPY','Back End Dev','2023'],
   para: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  parapop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries but also the leap into electronic typesetting remaining essent',
   tech: ['HTML','CSS','JavaScript'],
   source:'https://github.com/MasumaJaffery/Portfolio',
   livelink: 'https://masumajaffery.github.io/Portfolio/'
@@ -88,15 +92,15 @@ for(let i = 0; i < Projects.length; i += 1) {
   show(i);
 }
 //Pop Up Configurations;
-const PopUpWindow = document.getElementById('PopupDetails');
+const PopUpWindow = document.getElementById('PopupDetials');
 
 function PopUp(index) {
-  const project = Projects[index];
-
-  PopUpWindow.innerHTML = `
-    <div class="Modal hidden">
+  const project = Projects[index]; 
+  PopUpWindow.innerHTML =  `
+  <div class = "Modal-bg">
+  <div class="Modal">
       <div class="Modal-Title">
-        <h3 class="heading">${project.title}</h3>
+        <h2 class="heading">${project.title}</h2>
         <button type="button" class="close-btn">
           <svg width="14" height="14" viewBox="0 0 14 14" class="close-popup" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path class="close-popup" fill-rule="evenodd" clip-rule="evenodd" d="M0.292893 0.292893C0.683417 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L7 5.58579L12.2929 0.292893C12.6834 -0.0976311 13.3166 -0.0976311 13.7071 0.292893C14.0976 0.683417 14.0976 1.31658 13.7071 1.70711L8.41421 7L13.7071 12.2929C14.0976 12.6834 14.0976 13.3166 13.7071 13.7071C13.3166 14.0976 12.6834 14.0976 12.2929 13.7071L7 8.41421L1.70711 13.7071C1.31658 14.0976 0.683417 14.0976 0.292893 13.7071C-0.0976311 13.3166 -0.0976311 12.6834 0.292893 12.2929L5.58579 7L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683417 0.292893 0.292893Z" fill="#67798E"/>
@@ -113,7 +117,8 @@ function PopUp(index) {
       <div class="work-img">
         <img class="img1" src="${project.img}">
       </div>
-      <p>${project.para}</p>
+      <div class = 'desktop-pop'>
+      <p>${project.parapop}</p>
       <div class="tags">
         <ul class="hash">
           <li class="HTML">${project.tech[0]}</li>
@@ -121,12 +126,14 @@ function PopUp(index) {
           <li class="JavaScript">${project.tech[2]}</li>
         </ul>
       </div>
+      <hr>
       <div class="modal-buttons">
-        <button type="button" class="see-live"><a href="${project.livelink}">See Live <img src="Images/livelink.svg"></a></button>
-        <button type="button" class="see-source"><a href="${project.source}">See Source <img src="Images/GitHub.png"></a></button>
+      <a href="${project.livelink}"><button type="button" class="see-live">See Live<img src="Images/livelink.svg"></button></a>
+      <a href="${project.source}"><button type="button" class="see-source">See Source<img src="Images/blue.png"></button></a>
       </div>
-    </div>`;
-
+    </div>
+    </div>
+    </div>` ;
   // Add event listener to close button
   const closeButton = PopUpWindow.querySelector('.close-btn');
   closeButton.addEventListener('click', () => {
